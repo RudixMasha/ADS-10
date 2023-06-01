fad1927c99647aa97d9f64a731e14dccfa11534a
RudixMasha <116712267+RudixMasha@users.noreply.github.com>
2023-06-01 20:06:58 +0300

    Update tree.h

--- include/tree.h
+++ include/tree.h
@@ -16,13 +16,13 @@ class Tree {
       return;
     if (root->smthVal != '*') {
       for (auto k = per.begin(); k != per.end(); k++)
-        if (*k = root->smthVal) {
+        if (*k == root->smthVal) {
           per.erase(k);
           break;
         }
     }
     for (size_t i = 0; i < per.size(); i++)
-      root->greList.push_back(new Node())
+      root->greList.push_back(new Node());
     for (size_t i = 0; i < root->greList.size(); i++)
       root->greList[i]->smthVal = per[i];
     for (size_t i = 0; i < root->greList.size(); i++)
@@ -30,7 +30,7 @@ class Tree {
   }
   std::vector<std::string> permutation;
   void prm(Node* root, std::string symbol = "") {
-    if (root->greList.size()) {
+    if (!root->greList.size()) {
       symbol += root->smthVal;
       permutation.push_back(symbol);
     }
